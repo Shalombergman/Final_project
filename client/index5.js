db1af7507070cab67265a66a73acfdb5f008056e
Moshetzelniker <x0583200094@gmail.com>
2024-06-20 15:28:50 +0300

    added footer

---
+++ client/index5.js
@@ -0,0 +1,17 @@
+function enrollmentFunction (){
+    window.location.href = 'index0.html'
+}
+function footerDoctors (){
+    window.location.href = 'index2.html'
+}
+function footerSpecializations (){
+    window.location.href = 'index2.html'
+}
+function footerLocatio (){
+    window.location.href = 'index5.html'
+}
+document.getElementById('footerEnrollment').addEventListener('click',enrollmentFunction)
+document.getElementById('footerDoctors').addEventListener('click',footerDoctors)
+document.getElementById('footerSpecializations').addEventListener('click',footerSpecializations)
+document.getElementById('footerLocation').addEventListener('click',footerLocatio)
+
